01251c0ad0997f188c5034c700c3f79c720030c2
Bhanuka Mahanama <mahanaam94@gmail.com>
2020-01-03 23:35:33 -0500

    Code updates and improvements

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import numpy as np
 from heuristic_faces import HeuristicFaceClassifier
-
+import pandas as pd
 
 def run():
     heuristic_clf = HeuristicFaceClassifier()
@@ -11,6 +11,8 @@ def run():
     output_data_path = os.getcwd() + "\datasets\columbia_heuristic_gazes\data\\"
 
     dot_position_file = open(output_data_path + 'dot_position.dat', 'a')
+
+    dataframe = pd.DataFrame([], columns=['subject', 'l_eye_x', 'l_eye_y', 'l_eye_s', 'l_eye_px', 'l_eye_py', 'r_eye_x', 'r_eye_y', 'r_eye_s', 'r_eye_px', 'r_eye_py', 'g_v', 'g_h'])
 
     count = 0
     eye_w = []
@@ -48,28 +50,43 @@ def run():
                         pupil = eye["pupil"]
                         cv2.circle(crop, (x + pupil[0], y + pupil[1]), 7, (255, 0, 0), 2)
 
-                    cv2.imshow('crop', crop)
-                    cv2.imwrite('crop.jpg', crop)
-
-                    cv2.waitKey()
-                    break
+                    # print(face)
+                    # cv2.imshow('crop', crop)
+                    # cv2.imwrite('crop.jpg', crop)
                     # cv2.waitKey()
-                    #
-                    # image_data = image.split(".")[0].split("_")
-                    # subject = int(image_data[0])
-                    # dot_position = int(image_data[2].replace("P", ""))
-                    # camera_v_position = int(image_data[3].replace("V", ""))
-                    # camera_h_position = int(image_data[4].replace("H", ""))
+
+                    image_data = image.split(".")[0].split("_")
+                    subject = int(image_data[0])
+                    dot_position = int(image_data[2].replace("P", ""))
+                    camera_v_position = int(image_data[3].replace("V", ""))
+                    camera_h_position = int(image_data[4].replace("H", ""))
                     #
                     # cv2.imwrite(output_image_path + str(count) + '.jpg', crop)
                     # dot_position_file.write(str(dot_position) + "\n")
                     #
-                    print(count)
+
+                    face_size = face['face'][2]
+                    dataframe = dataframe.append({
+                        'subject': subject,
+                        'l_eye_x': face['eyes'][0]['eye'][0]/face_size,
+                        'l_eye_y': face['eyes'][0]['eye'][1]/face_size,
+                        'l_eye_s': face['eyes'][0]['eye'][2]/face_size,
+                        'l_eye_px': face['eyes'][0]['pupil'][0]/face_size,
+                        'l_eye_py': face['eyes'][0]['pupil'][1]/face_size,
+                        'r_eye_x': face['eyes'][1]['eye'][0]/face_size,
+                        'r_eye_y': face['eyes'][1]['eye'][1]/face_size,
+                        'r_eye_s': face['eyes'][1]['eye'][2]/face_size,
+                        'r_eye_px': face['eyes'][1]['pupil'][0]/face_size,
+                        'r_eye_py': face['eyes'][1]['pupil'][1]/face_size,
+                        'g_v': camera_v_position,
+                        'g_h': dot_position - camera_h_position
+                    }, ignore_index=True)
+
                     count = count + 1
+                    print(count)
 
-                # print(image_data)
-
-    dot_position_file.close()
+    # dot_position_file.close()
+    dataframe.to_csv('data.csv')
     print(min(eye_h))
     print(min(eye_w))
 
--- realtime.py
+++ realtime.py
@@ -1,10 +1,14 @@
 import numpy as np
 import cv2
 from heuristic_faces import HeuristicFaceClassifier
+import pickle
+import pandas as pd
 
 cap = cv2.VideoCapture(0)
 clf = HeuristicFaceClassifier()
+f = open("horizontal_gaze.pkcls", "rb")
 
+model = pickle.load(f)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -24,6 +28,17 @@ while(True):
             pupil = eye["pupil"]
             cv2.circle(gray, (ex + pupil[0], ey + pupil[1]), 7, (255, 0, 0), 1)
 
+        face_size = face['face'][2]
+        dataframe = pd.DataFrame({
+            'l_eye_x': face['eyes'][0]['eye'][0] / face_size,
+            'l_eye_s': face['eyes'][0]['eye'][2] / face_size,
+            'l_eye_px': face['eyes'][0]['pupil'][0] / face_size,
+            'r_eye_x': face['eyes'][1]['eye'][0] / face_size,
+            'r_eye_s': face['eyes'][1]['eye'][2] / face_size,
+            'r_eye_px': face['eyes'][1]['pupil'][0] / face_size}, index=[0])
+
+        prediction = model.predict(dataframe)[0]
+        cv2.putText(gray, "Direction : " + str(prediction), (x, y), thickness=2, fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 0, 0))
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -31,3 +46,5 @@ while(True):
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
+a = input("Test")
